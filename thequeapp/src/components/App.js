3e98f8c8e9da322c5f4ff60b547bf6350dd08973
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2023-01-21 11:03:08 +0100

    Nettoyage projet

---
+++ thequeapp/src/components/App.js
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="application">
+      <h1>Hello, début de projet.</h1>
+    </div>
+  );
+}
+
+export default App;
--- thequeapp/src/index.js
+++ thequeapp/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// == Import : npm
+import React from 'react'; // couche 1
+import { render } from 'react-dom'; // couche 2
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// == Import : local
+
+// Composants
+import App from './components/App';
+import './styles.css'
+
+// == Render
+
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = (
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+const target = document.getElementById('root');
+
+// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
+render(rootReactElement, target);

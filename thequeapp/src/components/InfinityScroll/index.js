318c948224768856978ddbf478da1c9cefb7e0f1
Victor Greiveldinger <victorgreiveldinger@gmail.com>
2023-01-23 16:40:02 +0100

    WIP : axios

--- thequeapp/src/components/InfinityScroll/index.js
+++ thequeapp/src/components/InfinityScroll/index.js
@@ -1,26 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 
 import axios from 'axios';
 
 const InfinityScroll = () => {
-    const [datas, setDatas] = useState({});
+    const [datas, setDatas] = useState();
     
-    console.log(datas);
-
-   const images = axios.get('https://pixabay.com/api/', {
-    params: {
-      key: '33061097-1f52a54ccfe341c6e4f2da427'
-    }}).then(res => {
-      setDatas(res);
-    }).catch(error => {
-      console.log(error);
+    useEffect(() => {
+      axios.get('https://pixabay.com/api/', {
+        params: {
+            key: '33061097-1f52a54ccfe341c6e4f2da427'
+        }
+      }).then(res => {
+        const resultats = res.data.hits;
+          resultats.forEach(e => {
+            if(e.id >= 7720040 || e.id  <= 7720000){
+              console.log(e);
+              setDatas(e);
+            }
+          });
+      }).catch(error => {
+        console.log(error);
+      });
     });
-
-    console.log(datas);
+    
     return (
         <div className="infinity-scroll">
             <h2>Gallery infinie</h2>
+            <img src="https://cdn.pixabay.com/photo/2023/01/18/15/24/scented-candles-7727169_150.jpg" alt="ok" />
         </div>
     );
 };
